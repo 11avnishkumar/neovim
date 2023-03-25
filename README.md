d4a8e6aeb881f0eaf730da945dbdd7e31082a039
avnish kumar <11avnishkumar@gmail.com>
2023-03-25 18:02:33 +0530

    Neovim

---
+++ README.md
@@ -0,0 +1,3 @@
+# Neovim
+
+![Neovim_icon](https://user-images.githubusercontent.com/90856122/227717413-816cca82-e430-459f-851e-fbb419318d5d.png)
---
+++ lua/user/treesitter.lua
@@ -0,0 +1,21 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+configs.setup({
+  ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "css" }, -- list of language that will be disabled
+	},
+  autotag = {
+    enable = true,
+  },
+	autopairs = {
+		enable = true,
+    filetypes = { "html",".htm","xml","tsx","js","jsx" },
+	},
+	indent = { enable = true, disable = { "python", "css" } },
+})
